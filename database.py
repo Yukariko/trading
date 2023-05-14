81e1bcbfd2e86de4059d50e3718df1e94317243a
yukariko <yukariko@github.com>
2023-05-15 00:56:14 +0900

    Implement database

--- database.py
+++ database.py
@@ -3,9 +3,14 @@ from pykrx import bond
 import time
 
 dl = stock.get_market_ticker_list(market="ALL")
-#df = stock.get_market_ohlcv("20230511", market="ALL")
+filePath = './data/list.csv'
+with open(filePath, 'w') as lf:
+    lf.write('\n'.join(dl))
 
 for ticker in dl:
-    df = stock.get_market_ohlcv("20220501", "20230511", ticker)
-    df.to_csv(f"./data/{ticker}.csv", sep=' ')
+    df = stock.get_market_ohlcv("20210512", "20230512", ticker)
+    print(df.columns)
+    df.columns = ['open_price', 'high_price', 'low_price', 'close_price', 'volume', 'amount', 'performance']
+    df.index.names = ['date']
+    df.to_csv(f"./data/{ticker}.csv", mode='a',  sep=' ')
     time.sleep(3)
--- src/database.rs
+++ src/database.rs
@@ -1,41 +1,91 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, cmp::Ordering};
 use serde::Deserialize;
 use std::fs::File;
+use std::iter::Skip;
+use std::slice::Iter;
 
 #[derive(Debug, Deserialize)]
 pub struct Column {
-    date : String,
-    open_price : u32,
-    high_price : u32,
-    low_price : u32,
-    close_price : u32,
-    volume : u32,
-    volume_cash : u64,
-    performance : f32,
+    pub date : String,
+    pub open_price : i32,
+    pub high_price : i32,
+    pub low_price : i32,
+    pub close_price : i32,
+    pub volume : u32,
+    pub amount : u64,
+    pub performance : f32,
 }
 
-pub struct DataBase<'a> {
-    db : HashMap<&'a str, Vec<Column>>,
+pub struct DataBase {
+    db : HashMap<String, Vec<Column>>,
+    pub stock_list : Vec<String>,
 }
 
-impl<'a> DataBase<'a> {
-    pub fn new() -> DataBase<'a> {
+impl DataBase {
+    pub fn new() -> DataBase {
         DataBase {
-            db : HashMap::new()
+            db : HashMap::new(),
+            stock_list : Self::load_list(),
         }
+    }
+
+    fn load_list() -> Vec<String> {
+        let file_path = "./data/list.csv";
+        let file = File::open(file_path).unwrap();
+        let mut reader = csv::Reader::from_reader(file);
+        let mut records : Vec<String> = Vec::new();
+        for record in reader.records() {
+            let record = record.unwrap();
+            if record.len() > 0 {
+                records.push(record[0].to_string());
+            }
+        }
+        records
+    }
+
+    // must be fix
+    pub fn get_date_list(&mut self) -> Vec<u32> {
+        let columns = Self::load_data("005930");
+        let mut results : Vec<u32> = Vec::with_capacity(columns.len());
+        for column in columns {
+            let date = column.date.replace("-", "");
+            let date_to_int = date.parse::<u32>().unwrap();
+            results.push(date_to_int);
+        }
+        results
+    }
+
+    // search date from date_list with lower bound
+    pub fn find_date(date_list: &Vec<u32>, date: u32) -> usize {
+        let position = date_list
+            .binary_search_by(|d| match d.cmp(&date) {
+                Ordering::Equal => Ordering::Greater,
+                ord => ord,
+            })
+            .or_else(|idx| Ok::<usize, usize>(idx))
+            .unwrap();
+        position
+    }
+
+    // search date from date_list with lower bound
+    pub fn iter_date(date_list: &Vec<u32>, date: u32) -> Skip<Iter<u32>> {
+        let position = Self::find_date(date_list, date);
+        date_list.iter().skip(position)
     }
 
     fn load_data(stock_no: &str) -> Vec<Column> {
         let file_path = format!("./data/{}.csv", stock_no);
         let file = File::open(file_path).unwrap();
-        let mut reader = csv::Reader::from_reader(file);
+        let mut reader = csv::ReaderBuilder::new()
+            .delimiter(b' ')
+            .from_reader(file);
         reader.deserialize::<Column>()
             .map(Result::unwrap)
             .collect()
     }
 
-    pub fn get_columns(&mut self, stock_no: &'a str) -> &Vec<Column> {
-        let columns = self.db.entry(stock_no).or_insert_with(|| DataBase::load_data(stock_no));
+    pub fn get_columns(&mut self, stock_no: &str) -> &Vec<Column> {
+        let columns = self.db.entry(stock_no.to_owned()).or_insert_with(|| { let mut data = DataBase::load_data(stock_no); data.reverse(); data });
         columns
     }
 }
